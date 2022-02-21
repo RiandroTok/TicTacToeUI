c866e2c7404497dac9e4798094d62e5b19d3d1f1
Riandro Proenca <riandro95@gmail.com>
2022-02-21 10:39:58 -0300

    Initial Commit

---
+++ TicTacToeUI/TicTacToeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeUIApp.swift
+//  TicTacToeUI
+//
+//  Created by Riandro Proença on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
